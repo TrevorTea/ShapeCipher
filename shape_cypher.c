8207b29adf6338ae8a5557b1593ddf3fd66f6f69
Trevor Thomas <trevort@trevortm4.local>
2025-04-20 13:07:54 -0500

    feature - Adds encrypting function.

--- shape_cypher.c
+++ shape_cypher.c
@@ -10,6 +10,9 @@
 ///                                 DEFINES                                 ///
 ///////////////////////////////////////////////////////////////////////////////
 
+#define CEIL(x) ((x - (int)x) ? (int)(x + 1) : (int)(x))
+#define MAXFILESIZE (1<<16)
+
 ///////////////////////////////////////////////////////////////////////////////
 ///                                  ENUMS                                  ///
 ///////////////////////////////////////////////////////////////////////////////
@@ -22,8 +25,29 @@
 ///                                FUNCTIONS                                ///
 ///////////////////////////////////////////////////////////////////////////////
 
-char * encode(int key, char* msg) {
-    return NULL;
+char * encrypt(int key, char* msg) {
+    unsigned int len = strlen(msg);
+    char** strs = (char **) malloc(sizeof(char*) * key);
+    char* res = (char *) malloc(sizeof(char) * len);
+    for (unsigned int i = 0; i < key; i++) {
+        strs[i] = (char*) malloc(sizeof(char) * (CEIL((float)len / key)));
+    }
+    for(unsigned int i = 0; i < len; i++) {
+        strs[i%key][i/key] = msg[i];
+    }
+    for(unsigned int i = 0; i < key; i++) {
+        strcat(res, strs[i]);
+    }
+    // TODO Handle wrapping around the back of the alphabet.
+    for(unsigned int i = 0; i < len; i++) {
+        res[i] = res[i] + key;
+    }
+
+    for (unsigned int i = 0; i < key; i++) {
+        free(strs[i]);
+    }
+    free(strs);
+    return res;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -31,16 +55,34 @@ char * encode(int key, char* msg) {
 ///////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char** argv) {
-    if (argc < 2 || argc > 3) {
+    if (argc < 3 || argc > 4) {
         fprintf(stderr, "Improper usage, only one arg can be provided.\n");
     }
 
     char * msg = NULL;
 
-    if (strstr(argv[1], ".txt")) {
-        msg = fopen(argv[1], "r");
+    if (strstr(argv[2], ".txt")) {
+        FILE* inp = fopen(argv[2], "r");
+        fseek(inp, 0, SEEK_END);
+        unsigned int sz = ftell(inp);
+        rewind(inp);
+        msg = (char*) malloc(sizeof(char) * (sz + 1));
+        msg = fgets(msg, sz, inp);
+        msg[sz] = '\0';
     }
     else {
-        msg = argv[1];
+        msg = argv[2];
     }
+
+    char* encrypted = encrypt(atoi(argv[1]), msg);
+
+    FILE* out = NULL;
+    if (argc == 4) {
+        out = fopen(argv[3], "w");
+    }
+    else out = stdout;
+
+    fprintf(out, "%s", encrypted);
+    fprintf(out, "\n");
+    return 0;
 }
